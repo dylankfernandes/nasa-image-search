a8694bbc6aa588b925358ac5e0f1d2d954f52828
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-03-11 13:54:13 -0400

    finished initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => (
+  <nav className="nav">
+    <div className="nav__menu">
+      <ul>
+        <li>Search</li>
+        <li>Favorites</li>
+        <li></li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Navbar
---
+++ src/config/api-key.js
@@ -0,0 +1,3 @@
+const API_KEY = 'lmX3G3ayNWm2S5NmBbNDg343cweWVWOGqqRgl6Vy'
+
+export default API_KEY;

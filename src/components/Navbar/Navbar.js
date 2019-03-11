dcf254a8f0c2d15eaecb498b98cc3902dae4248c
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-03-11 16:12:07 -0400

    finished initial setup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Navbar = () => (
-  <nav className="nav">
-    <div className="nav__menu">
-      <ul>
-        <li>Search</li>
-        <li>Favorites</li>
-        <li></li>
-      </ul>
+  <nav className="navbar">
+    <div className="navbar__header">
+      <h1>NASA Image Search</h1>
+    </div>
+    <div className="navbar__search">
+      <label htmlFor=""></label>
+      <input type="text" />
     </div>
   </nav>
 )

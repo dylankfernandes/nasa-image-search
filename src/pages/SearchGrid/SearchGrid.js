550816f1ce2d6412cfb690008f94f3b78c89d710
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-03-15 14:39:17 -0400

    breaking off into separate branch to fit with API

--- src/pages/SearchGrid/SearchGrid.js
+++ src/pages/SearchGrid/SearchGrid.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import ImageGrid from '../../components/ImageGrid/'
 import Header from '../../components/Header'
+import API_KEY from '../../config/api-key'
 
 class SearchGrid extends Component {
+
+  state = {
+    data: ''
+  }
+
+  componentWillMount() {
+    var url = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY
+    axios.get(url)
+      .then(res => {
+        this.setState({
+          data: res.data,
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="search-grid">

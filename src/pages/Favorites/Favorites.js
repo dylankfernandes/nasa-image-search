4d695b1b75aa79bf5e907d9fbe4df5bf617be985
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-03-17 16:49:05 -0400

    finished revamping components - need to add routing and favorites source code

---
+++ src/pages/Favorites/Favorites.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from '../../components/Header';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="favorites">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Favorites
